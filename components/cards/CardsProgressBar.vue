a070fd236e80a8e07ccacf0bcec1eaec3959fee5
J0RdN-w <yordanoswuletaw@gmail.com>
2022-08-25 18:05:42 +0300

    update progress bar

--- components/cards/CardsProgressBar.vue
+++ components/cards/CardsProgressBar.vue
@@ -13,7 +13,7 @@ const props = defineProps({
         <h5 class="mt-3 text-xs font-medium">{{subSkill.subtitle}}</h5>
         <div class="flex items-center gap-3 mt-2">
             <div  class="w-4/5 h-[0.4rem] bg-gray-400 rounded-full overflow-hidden">
-                <div :style="`width: ${subSkill.sublevel}%;`" class="h-2 bg-primary rounded-r-full">
+                <div :style="`width: ${subSkill.sublevel}%;`" class="h-[0.4rem] bg-primary rounded-r-full">
                 </div>
             </div>
             <h6 class="text-xs font-medium">{{subSkill.sublevel}}%</h6>
