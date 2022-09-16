c0ab557f524ea04c8f69e94c86a1170a5fa7f916
J0RdN-w <yordanoswuletaw@gmail.com>
2022-09-16 10:02:06 +0300

    update custom.vue

--- components/icons/IconsUCurve.vue
+++ components/icons/IconsUCurve.vue
@@ -3,17 +3,20 @@ const theme = useTheme()
 </script>
 <template>
     <svg class="min-w-full" viewBox="0 0 1440 270" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g filter="url(#filter0_b_448_678)">
-            <path d="M699 148L1440 0V270H0V174C77 302 513 188 699 148Z" :fill="theme ? '#1A1C22' : '#F5F5F5'"
+        <path d="M699 148L1440 0V270H0V174C77 302 513 188 699 148Z" :fill="theme ? '#1A1C22' : '#F5F5F5'"
                 :fill-opacity="theme ? '0.4' : '0.4'" />
+        <path d="M695 160L1440 20V270H0V174C77 302 509 200 695 160Z" :fill="theme ? '#1A1C22' : '#F5F5F5'"
+                :fill-opacity="theme ? '0.5' : '0.5'" />     
+        <path d="M709 174L1440 42V270H0V174C77 302 523 214 709 174Z" :fill="theme ? '#1A1C22' : '#F5F5F5'"
+                :fill-opacity="theme ? '0.7' : '0.6'" />   
+        <!-- <g filter="url(#filter0_b_448_678)">
+            
         </g>
         <g filter="url(#filter1_b_448_678)">
-            <path d="M695 160L1440 20V270H0V174C77 302 509 200 695 160Z" :fill="theme ? '#1A1C22' : '#F5F5F5'"
-                :fill-opacity="theme ? '0.5' : '0.5'" />
+            
         </g>
         <g filter="url(#filter2_b_448_678)">
-            <path d="M709 174L1440 42V270H0V174C77 302 523 214 709 174Z" :fill="theme ? '#1A1C22' : '#F5F5F5'"
-                :fill-opacity="theme ? '0.7' : '0.6'" />
+            
         </g>
         <defs>
             <filter id="filter0_b_448_678" x="-4" y="-4" width="1448" height="278" filterUnits="userSpaceOnUse"
@@ -37,6 +40,6 @@ const theme = useTheme()
                 <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_448_678" />
                 <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_448_678" result="shape" />
             </filter>
-        </defs>
+        </defs> -->
     </svg>
 </template>
